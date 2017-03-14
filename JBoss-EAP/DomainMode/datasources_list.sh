a7d5e7ffbc5109d3559eb0d999a1de474bf6b953
Robson Watt <rwattcha@redhat.com>
2017-03-14 09:23:07 -0300

    uso de variables script

--- JBoss-EAP/DomainMode/datasources_list.sh
+++ JBoss-EAP/DomainMode/datasources_list.sh
@@ -2,7 +2,7 @@
 #set -x
 
 # Obtiene todos los profiles del domain
- _PROFILES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command=":read-children-names(child-type=profile)" | grep '        "' |sed 's/        \"//g'| sed 's/\"//g'|sed 's/,//g')
+ _PROFILES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command=":read-children-names(child-type=profile)" | grep '        "' |sed 's/        \"//g'| sed 's/\"//g'|sed 's/,//g')
 
  # o consultamos por los profiles que nos interesen
  _PROFILES=default
@@ -11,10 +11,10 @@ echo "Profiles a revisar: " $_PROFILES
 echo "$_PROFILES"|while read _PROFILE
   do
     echo "Obteniendo datasources para Profile " $_PROFILE
-    _DATASOURCES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
+    _DATASOURCES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
     #si _DATASOURCES está vacío se trata de ExampleDS
     if [ “$_DATASOURCE” == “” ]; then
-       _DATASOURCES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep result | sed 's/    \"result\" => \["//g' | sed 's/\"]//g')
+       _DATASOURCES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep result | sed 's/    \"result\" => \["//g' | sed 's/\"]//g')
     fi
     echo $_DATASOURCES
 done
--- JBoss-EAP/DomainMode/datasources_test_connection.sh
+++ JBoss-EAP/DomainMode/datasources_test_connection.sh
@@ -2,7 +2,7 @@
 #set -x
 
 # Obtiene todos los profiles del domain
-_PROFILES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command=":read-children-names(child-type=profile)" | grep '        "' |sed 's/        \"//g'| sed 's/\"//g'|sed 's/,//g')
+_PROFILES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command=":read-children-names(child-type=profile)" | grep '        "' |sed 's/        \"//g'| sed 's/\"//g'|sed 's/,//g')
 
 # o consultamos por los profiles que nos interesen
 _PROFILES=default
@@ -11,20 +11,20 @@ echo "Profiles a revisar: " $_PROFILES
 echo "$_PROFILES"|while read _PROFILE
  do
    echo "Obteniendo datasources para Profile " $_PROFILE
-   _DATASOURCES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
+   _DATASOURCES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
 
    #si _DATASOURCES está vacío se trata de ExampleDS
    if [ “$_DATASOURCES” == “” ]; then
-       _DATASOURCES=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep result | sed 's/    \"result\" => \["//g' | sed 's/\"]//g')
+       _DATASOURCES=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/profile=$_PROFILE/subsystem=datasources:read-children-names(child-type=data-source)" | grep result | sed 's/    \"result\" => \["//g' | sed 's/\"]//g')
    fi
 
-   _HOSTS=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command=":read-children-names(child-type=host)"  | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
+   _HOSTS=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command=":read-children-names(child-type=host)"  | grep '        "' | sed 's/        \"//g' | sed 's/\",//g' | sed 's/"//g')
 
    echo "$_HOSTS"|while read  _HOST
         do
-                _SERVERS=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/host=$_HOST:read-children-names(child-type=server)"  | grep '        "'| sed 's/        "//g' | sed 's/"//g' | sed 's/,//g')
+                _SERVERS=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/host=$_HOST:read-children-names(child-type=server)"  | grep '        "'| sed 's/        "//g' | sed 's/"//g' | sed 's/,//g')
                 if [ “$_SERVER == “” ]; then
-                  _SERVERS=$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/host=$_HOST:read-children-names(child-type=server)"  | grep result | cut -d '[' -f2 | sed 's/"//g' | sed 's/]//g')
+                  _SERVERS=$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/host=$_HOST:read-children-names(child-type=server)"  | grep result | cut -d '[' -f2 | sed 's/"//g' | sed 's/]//g')
                 fi
                 echo "$_DATASOURCES"|while read _DS
                         do
@@ -32,7 +32,7 @@ echo "$_PROFILES"|while read _PROFILE
                                   do
                                     echo "Test Connection Host $_HOST, Servidor $_SERVER, Datasource $_DS"
                                     echo "/host="$_HOST"/server="$_SERVER"/subsystem=datasources/data-source="$_DS":test-connection-in-pool"
-                                    echo -e "$(/opt/EAP-Ansible-POC/domainController/bin/jboss-cli.sh -c controller=192.168.0.69:9999 --command="/host=$_HOST/server=$_SERVER/subsystem=datasources/data-source=$_DS:test-connection-in-pool")"
+                                    echo -e "$($1/bin/jboss-cli.sh -c controller=$2:9999 --command="/host=$_HOST/server=$_SERVER/subsystem=datasources/data-source=$_DS:test-connection-in-pool")"
                                   done
                         done
         done
